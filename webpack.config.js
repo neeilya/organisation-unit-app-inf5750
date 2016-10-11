68835fec4a781575793f21936d51caa7684b7e96
Ilya Nee <neeilya@mail.ru>
2016-10-11 18:22:46 +0200

    Adjusted webpack config to handle jsx, set entry file to app.jsx

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     // The entry file of our app, where the app should start.
-    entry: './src/app.js',
+    entry: './src/app.jsx',
     output: {
         // The filename of the bundle. This file will contain all our code from all the separate files bundled into one .js file.
         filename: 'app.js'
@@ -12,5 +12,15 @@ module.exports = {
         port: 8081,
         // Serve the files from the src directory. So http://localhost:8081 will load index.html from the ./src directory.
         contentBase: './src',
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+                query: { presets: ['react'] }
+            }
+        ],
     }
 };
